a0449606e810207b3adc166b89fb0249fbfc42f5
emrakyz <emreakyuz_2@hotmail.com>
2026-01-16 11:25:28 +0100

    Fix Windows pipe handling

--- src/y4m.rs
+++ src/y4m.rs
@@ -1,9 +1,7 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
-use std::os::unix::io::FromRawFd;
+use std::io::{BufRead, BufReader, IsTerminal, Read, Stdin};
 
 pub fn is_pipe() -> bool {
-    unsafe { libc::isatty(libc::STDIN_FILENO) == 0 }
+    !std::io::stdin().is_terminal()
 }
 
 pub struct Y4mInfo {
@@ -13,7 +11,7 @@ pub struct Y4mInfo {
 }
 
 pub struct PipeReader {
-    reader: BufReader<File>,
+    reader: BufReader<Stdin>,
     pub frame_size: usize,
     frame_header: [u8; 6],
 }
@@ -29,8 +27,8 @@ impl PipeReader {
     pub fn skip_frames(&mut self, count: usize) {
         let mut discard = vec![0u8; self.frame_size];
         for _ in 0..count {
-            self.reader.read_exact(&mut self.frame_header).unwrap();
-            self.reader.read_exact(&mut discard).unwrap();
+            let _ = self.reader.read_exact(&mut self.frame_header);
+            let _ = self.reader.read_exact(&mut discard);
         }
     }
 }
@@ -40,8 +38,8 @@ pub fn init_pipe() -> Option<(Y4mInfo, PipeReader)> {
         return None;
     }
 
-    let file = unsafe { File::from_raw_fd(libc::STDIN_FILENO) };
-    let mut reader = BufReader::new(file);
+    let stdin = std::io::stdin();
+    let mut reader = BufReader::new(stdin);
     let mut header = String::new();
     reader.read_line(&mut header).unwrap();
 
