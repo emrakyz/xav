6db1c9279b3fa6150f84b38ea266110748a53a2e
emrakyz <emreakyuz_2@hotmail.com>
2026-02-12 01:03:44 +0100

    Improve building with library

--- build.rs
+++ build.rs
@@ -30,7 +30,20 @@ fn main() {
 
     if cfg!(feature = "libsvtav1") {
         let home = env::var("HOME").expect("HOME environment variable not set");
-        println!("cargo:rustc-link-search=native={home}/.local/src/SVT-AV1/Bin/Release");
+        let search_paths = [
+            format!("{home}/.local/src/svt-av1-hdr/Bin/Release"),
+            format!("{home}/.local/src/SVT-AV1/Bin/Release"),
+            "/usr/lib64".to_string(),
+            "/usr/lib".to_string(),
+            "/lib64".to_string(),
+            "/lib".to_string(),
+        ];
+        for path in &search_paths {
+            if std::path::Path::new(&format!("{path}/libSvtAv1Enc.a")).exists() {
+                println!("cargo:rustc-link-search=native={path}");
+                break;
+            }
+        }
         println!("cargo:rustc-link-lib=static=SvtAv1Enc");
     }
 }
