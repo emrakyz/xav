f50b9e4bb206db5975f193f6df615cd2c7d09598
emrakyz <emreakyuz_2@hotmail.com>
2026-02-23 02:18:22 +0100

    Apply fixes

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,13 @@
-//! This module contains various generic utilities that
-//! are not specific to any area of xav's workflow.
-
 #[cfg(not(debug_assertions))]
 use std::hint::unreachable_unchecked;
 
-/// In debug mode, this function calls the `unreachable!()` macro to panic.
-/// In release mode, this function calls the `unreachable_unchecked` function.
 #[inline(always)]
 #[allow(clippy::inline_always, reason = "thin compiler-elided wrapper")]
-pub const fn debug_unreachable() -> ! {
+#[allow(
+    clippy::panic,
+    reason = "debug-only panic for catching logic errors in tests"
+)]
+pub const fn assume_unreachable() -> ! {
     #[cfg(debug_assertions)]
     unreachable!();
 
