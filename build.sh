2f5414a182eadc02854df563b6b1212fa5ef58f1
emrakyz <emreakyuz_2@hotmail.com>
2026-08-15 20:31:34 +0200

    fix split units

--- build.sh
+++ build.sh
@@ -704,8 +704,8 @@ build_avm() {
                 -DCMAKE_BUILD_TYPE=Release \
                 -DCMAKE_C_COMPILER="${CC}" \
                 -DCMAKE_CXX_COMPILER="${CXX}" \
-                -DCMAKE_C_FLAGS="${CFLAGS//-fwhole-program-vtables/}" \
-                -DCMAKE_CXX_FLAGS="${CXXFLAGS//-fwhole-program-vtables/}" \
+                -DCMAKE_C_FLAGS="${CFLAGS}" \
+                -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
                 -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -flto=thin" \
                 -DBUILD_SHARED_LIBS=OFF \
                 -DENABLE_APPS=0 \
@@ -752,7 +752,7 @@ setup_toolchain() {
         export VULKAN_SDK="${BUILD_DIR}/vulkan/install"
 
         export COMMON_FLAGS="-O3 -ffast-math -march=native -mtune=native \
-	-fwhole-program-vtables -flto=thin -fno-semantic-interposition \
+	-flto=thin -fno-semantic-interposition \
 	-fno-stack-protector -fno-stack-clash-protection -fno-sanitize=all \
 	-fno-dwarf2-cfi-asm -fno-pic -fno-pie -fno-unwind-tables \
 	-fno-asynchronous-unwind-tables -fno-plt -fno-stack-check \
