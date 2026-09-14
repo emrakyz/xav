8a803a8e6b053c66ae85e831260ab5c623aa77e1
emrakyz <emreakyuz_2@hotmail.com>
2026-09-14 17:37:58 +0200

    reduce instructions and accuracy for sampling

--- asm/avx2/crop/samp.asm
+++ asm/avx2/crop/samp.asm
@@ -2,20 +2,18 @@
 
 SECTION_RODATA 32
 ALIGN 32
-init: dd 0.07142857142857142, 0.14285714285714285, 0.21428571428571427, 0.2857142857142857
-      dd 0.35714285714285715, 0.42857142857142855, 0.5, 0.5714285714285714
-      dd 0.6428571428571429, 0.7142857142857143, 0.7857142857142857, 0.8571428571428571
-      dd 0.9285714285714286, 1.0, 1.0714285714285714, 1.1428571428571428
+init: dd 73, 146, 219, 292, 365, 438, 511, 584
+      dd 657, 730, 803, 876, 949, 1022, 1095, 1168
 
 SECTION .text
 INIT_YMM avx2
 cglobal calc_samp_frames, 2, 2, 3, tot, out
-    vcvtsi2ss    xmm0, xmm0, totd
-    vbroadcastss ymm0, xmm0
-    vmulps       ymm1, ymm0, [rel init]
-    vmulps       ymm2, ymm0, [rel init + 32]
-    vcvtps2dq    ymm1, ymm1
-    vcvtps2dq    ymm2, ymm2
+    vmovd        xmm0, totd
+    vpbroadcastd ymm0, xmm0
+    vpmulld      ymm1, ymm0, [rel init]
+    vpmulld      ymm2, ymm0, [rel init + 32]
+    vpsrld       ymm1, ymm1, 10
+    vpsrld       ymm2, ymm2, 10
     vmovdqu      [outq],      ymm1
     vmovdqu      [outq + 32], ymm2
     RET
